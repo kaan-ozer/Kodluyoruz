65d1a1d7e552b81344c27106e4632bd4f5e3b241
Kaan Özer <kaanozer35@gmail.com>
2023-03-02 00:55:04 +0300

    26-form-input-value.js + 27-form-bolum-sonu-egzersizi added

---
+++ JS-Section/26-form-input-value.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// let formDOM = document.querySelector(`#userForm`);
+// formDOM.addEventListener("submit", formSubmit);
+
+// function formSubmit(event) {
+//   //post geti engelledik bizim functionımıza göre tepki vericek
+//   event.preventDefault();
+//   console.log("islem gerceklesti");
+//   let scoreInputDOM = document.querySelector(`#score`);
+//   console.log(scoreInputDOM.value);
+//   localStorage.setItem("score", scoreInputDOM.value);
+// }
+// let bodyDOM = document.querySelector(`body`);
+// var myDiv = document.createElement("div");
+// bodyDOM.appendChild(myDiv);
+
+// var myInput = document.createElement("input");
+// myInput.setAttribute("id", "inputumuz");
+// myInput.setAttribute("type", "text");
+// myInput.setAttribute("value", "Buraya Yazın");
+// myDiv.appendChild(myInput);
+
+// var inputDeger = document.getElementById("inputumuz").value;
+// window.alert(inputDeger); // Değerimiz burada yazacak
+
+// function myFunction() {
+//   var x = document.getElementById("myInput").value;
+//   document.getElementById("myP").innerHTML = "Değerimiz:" + " " + x;
+// }

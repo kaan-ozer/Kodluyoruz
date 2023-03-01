ea1fd45c72d03ef9184d5155205ea13038ba55c3
Kaan Özer <kaanozer35@gmail.com>
2023-03-01 19:09:40 +0300

    24-localstoreage-bolum-sonu-egzersizi.js added

--- JS-Section/24-localstoreage-bolum-sonu-egzersizi.js
+++ JS-Section/24-localstoreage-bolum-sonu-egzersizi.js
@@ -1,1 +1,22 @@
 "use strict";
+"use strict";
+
+let counter = localStorage.getItem("counter")
+  ? Number(localStorage.getItem("counter"))
+  : 0;
+let counterDom = document.querySelector(`#counter`);
+let increaseDom = document.querySelector(`#increase`);
+let decreaseDom = document.querySelector(`#decrease`);
+
+counterDom.innerHTML = counter;
+
+increaseDom.addEventListener("click", clickEvent);
+decreaseDom.addEventListener("click", clickEvent);
+
+function clickEvent() {
+  console.log(typeof counter);
+  console.log(this.id);
+  this.id == "increase" ? (counter += 1) : (counter -= 1);
+  localStorage.setItem("counter", counter);
+  counterDom.innerHTML = counter;
+}
